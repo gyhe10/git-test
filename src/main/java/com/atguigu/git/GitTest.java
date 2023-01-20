0ae5e8fb3d09b6889a458c11c1a461d5b3c78dbf
gyhe10 <gyhe10@163.com>
2023-01-20 15:58:39 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @program: git-test
+ * @description:
+ * @author: gyhe10
+ * @create: 2023-01-20 15:53
+ **/
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
