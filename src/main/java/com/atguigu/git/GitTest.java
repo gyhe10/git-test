19b075fbe28b76b3b5a1030a985cafc571805625
gyhe10 <gyhe10@163.com>
2023-01-20 16:10:21 +0800

    master first commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git!2");
         System.out.println("hello git! hot-fix");
+        System.out.println("hello git! master");
     }
 }
